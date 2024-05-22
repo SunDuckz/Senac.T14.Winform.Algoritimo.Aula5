9b36b3096a158f14786cbcc794f2f2ac006c5998
SunDuckz <gui.bampi0@gmail.com>
2024-05-22 20:21:51 -0300

    Adição de 4 telas forms, adição de pacote de conteúdo e criação de tela de login

---
+++ WF.Algoritimo.ClasseEMetodo/Form1.cs
@@ -0,0 +1,40 @@
+namespace WF.Algoritimo.ClasseEMetodo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void airForm2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void foreverForm1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void foreverCheckBox1_CheckedChanged(object sender)
+        {
+
+        }
+
+        private void foreverLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void foreverGroupBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
